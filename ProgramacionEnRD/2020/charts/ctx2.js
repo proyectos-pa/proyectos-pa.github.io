6e28f4f03cbf3152fad23df6eb194e7a5ded3bb4
Gus Caff <caffaro.gustavo@gmail.com>
2020-07-20 01:51:31 -0400

    Update ProgramacionEnRD/2020/charts/ctx2.js

--- ProgramacionEnRD/2020/charts/ctx2.js
+++ ProgramacionEnRD/2020/charts/ctx2.js
@@ -2,11 +2,11 @@ var ctx2 = document.getElementById('pq2').getContext('2d');
 var chart = new Chart(ctx2, {
     type: 'horizontalBar',
     data: {
-      labels: ['SQL ', 'Javascript ', 'HTML/CSS ', 'C# ', 'Python ', 'Java ', 'PHP ', 'TypeScript ', 'R ', 'C++ ', 'VBA ', 'Bash ', 'Otro (especifique) ', 'C ', 'Dart ', 'MATLAB ', 'Kotlin ', 'Stata ', 'Ruby ', 'Eviews ', 'Go ', 'Swift ', 'Rust ', 'Objective-C ', 'Perl ', 'Assembly ', 'Haskell ', 'Scala '],
+      labels: ['SQL ', 'Javascript ', 'HTML/CSS ', 'C# ', 'Python ', 'Java ', 'PHP ', 'TypeScript ', 'R ', 'C++ ', 'VBA ', 'Bash ', 'C ', 'Dart ', 'MATLAB ', 'Kotlin ', 'Stata ', 'Ruby ', 'Eviews ', 'Go ', 'Swift ', 'Rust ', 'Objective-C ', 'Perl ', 'Assembly ', 'Haskell ', 'Scala '],
       datasets: [{
         backgroundColor: '#1dc4f7',
         borderColor: '#202529',
-        data: [67.1, 64.4, 63.8, 43.1, 40.1, 27, 25.6, 21.5, 12.5, 11.2, 8.1, 8, 7.3, 7.3, 3.8, 3.8, 3.5, 3.1, 2.8, 2.6, 1.7, 1.2, 1, 1, 0.7, 0.7, 0.5, 0.3]
+        data: [67.1, 64.4, 63.8, 43.1, 40.1, 27, 25.6, 21.5, 12.5, 11.2, 8.1, 8, 7.3, 3.8, 3.8, 3.5, 3.1, 2.8, 2.6, 1.7, 1.2, 1, 1, 0.7, 0.7, 0.5, 0.3]
       }]
     },
     plugins: [ChartDataLabels],
