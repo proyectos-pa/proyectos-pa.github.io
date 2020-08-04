273274c21715d3690103d95c25e5f589a1b92696
Gus Caff <caffaro.gustavo@gmail.com>
2020-08-04 09:06:19 -0400

    tabla gabinete abi

---
+++ BaseDatosFuncionarios/MinistrosAbinader/scripts/table_init.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#example').DataTable( {
+        ajax:           "../data/2500.txt",
+        deferRender:    true,
+        scrollY:        200,
+        scrollCollapse: true,
+        scroller:       true
+    } );
+} );
